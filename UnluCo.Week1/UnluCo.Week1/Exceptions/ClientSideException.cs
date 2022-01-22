152ac1d8c3fba931dc3b51f043d1f0fd815e5aca
Cem FINDIKLI <cemfindikli93@gmail.com>
2022-01-23 00:32:52 +0300

    Add extensions and custom middleware

---
+++ UnluCo.Week1/UnluCo.Week1/Exceptions/ClientSideException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UnluCo.Week1.Exceptions
+{
+    public class ClientSideException
+    {
+    }
+}
---
+++ UnluCo.Week1/UnluCo.Week1/Middlewares/UseCustomExceptionHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+
+namespace UnluCo.Week1.Middlewares
+{
+    public static class UseCustomExceptionHandler
+    {
+        public static void UserCustomException(this IApplicationBuilder app)
+        {
+            app.UseExceptionHandler(config =>
+            {
+                //Run sonlandırıcı bir middlewaredır. Buradan sonra akış geriye dönecek.
+                config.Run(async context =>
+                {
+                    context.Response.ContentType = "application/json";
+                    var exceptionFeature = context.Features.Get<IExceptionHandlerFeature>();
+                    var statusCode = 
+                }); 
+            });
+
+        }
+    }
+}
